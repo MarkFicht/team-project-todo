d658b972a416f42e5e0e9f647b7997f6bdc91c64
Bezsilny <julek200@gmail.com>
2018-09-15 00:05:38 +0200

    Adding tasks to list and showing them (atm in the same component)
    New tasks are being added to the list, however, there is no validation and there are no styles. Tasks are being shown within the same component.

--- src/AddTask/AddTask.js
+++ src/AddTask/AddTask.js
@@ -3,15 +3,47 @@ import './AddTask.css';
 
 class AddTask extends React.Component {
 
-    constructor(props) {
-        super(props);
 
-    }
+    state = {
+        tasks: [],
+        currentTask: ''
+    };
+
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    };
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            tasks: [...prevState.tasks, this.state.currentTask]
+        }))
+    };
 
     render() {
         return (
             <div>
-                AddTask
+                <div>AddTask</div>
+                <input
+                    type="text"
+                    value={this.state.currentTask}
+                    onChange={this.handleChange}
+                    id="currentTask"
+                />
+                <ul>
+                    {
+                        this.state.tasks.map(element => {
+                            return (
+                                <li key={element}>
+                                    {element}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                {<button onClick={this.handleClick}>Dodaj</button>}
             </div>
         );
     }
