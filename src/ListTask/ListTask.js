5d09063875deacdfc03167e0db6bfe1f19e1ea58
Bezsilny <julek200@gmail.com>
2018-09-16 22:12:38 +0200

    Enabled deleting tasks.

--- src/ListTask/ListTask.js
+++ src/ListTask/ListTask.js
@@ -8,16 +8,26 @@ class ListTask extends React.Component {
 
     }
 
+    removeTask(index) {
+        const tasks = this.props.values;
+        tasks.splice(index, 1);
+
+        this.setState({
+            tasks
+        })
+    }
+
     render() {
         return (
             <div>
                 ListTask
                 <ul>
                     {
-                      this.props.values && this.props.values.map(element => {
+                      this.props.values && this.props.values.map( (element, index) => {
                             return (
                                 <li key={element}>
                                     {element}
+                                    <button onClick={event => this.removeTask(index, event)}>Delete</button>
                                 </li>
                             )
                         })
